edaaeafcdd7f40ee8478e6073f32236ab833c372
MuathNahhas <muath.nahhas@yahoo.com>
2021-09-05 20:42:03 +0300

    Q10 part2

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -50,5 +50,6 @@ Q9) What is the difference between authentication and authorization?
 authentication:virification for user credinteals
 authorization:set of rules for user that what can and can not do 
 Q10) Why do we save some information as an environment variable (in .env file)?
+env:environmet using to store sensitive information and dont put in code 
 
 */
